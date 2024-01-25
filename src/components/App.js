fcf118d284ebe6f8148c99c4a8c7e3864dd7c9e2
76248usa <elsabecrous@Elsabes-MBP.lan>
2024-01-25 17:48:05 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,120 @@
+import { useEffect } from "react";
+import { useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import Ready from "./Ready";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import Finish from "./Finish";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "newAnswer":
+      const question = state.questions[state.index];
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOptions
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index, answer, points } = state;
+
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <Ready numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
+          </>
+        )}
+        {status === "finished" && (
+          <Finish points={points} maxPossiblePoints={maxPossiblePoints} />
+        )}
+      </Main>
+    </div>
+  );
+}
---
+++ src/components/Finish.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Finish({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints}(
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+}
+
+export default Finish;
